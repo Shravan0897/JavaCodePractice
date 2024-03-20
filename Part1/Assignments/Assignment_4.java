12173ec1a7cea9a261d9e731fcca4fe828f8b02d
Shravan Tomar <shravan@Shravans-MacBook-Air.local>
2024-03-20 10:29:01 +0530

    fix: added code piece to print number to be entered

--- Part1/Assignments/Assignment_4.java
+++ Part1/Assignments/Assignment_4.java
@@ -3,8 +3,11 @@ import java.util.*;
 public class Assignment_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number");
         int a = sc.nextInt();
+        System.out.println("Enter second number");
         int b = sc.nextInt();
+        System.out.println("Enter 1 for addition\nEnter 2 for Subtraction\nEnter 3 for Multiplication\nEnter 4 for Division\nEnter 5 for remainder");
         int calc = sc.nextInt();
         switch (calc) {
             case 1: System.out.println(a+b);
