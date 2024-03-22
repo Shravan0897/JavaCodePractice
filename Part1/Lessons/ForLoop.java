24f33c0a06d0e79afbcfac0e78dcbef990c303d5
Shravan Tomar <shravan@Shravans-MacBook-Air.local>
2024-03-22 22:15:39 +0530

    feat: added for loop assignment

---
+++ Part1/Lessons/ForLoop.java
@@ -0,0 +1,9 @@
+package Lessons;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int counter = 1; counter <= 100; counter++ ){
+            System.out.println(counter+":Hello World!");
+        }
+    }
+}
