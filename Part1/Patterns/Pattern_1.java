61f8ede3a90624f2ae77f5ef16dec5f4ff05a674
Shravan Tomar <shravan@Shravans-MacBook-Air.local>
2024-04-01 00:35:58 +0530

    feat: added some pattern assignments

---
+++ Part1/Patterns/Pattern_1.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class Pattern_1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+}
